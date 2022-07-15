567745979e7cb0368e94c1f5a61bd3ef37bf3bb2
Aviad <avi27200p@gmail.com>
2022-07-15 14:04:15 +0300

    Server side

---
+++ Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellcomClient
+{
+    public class Client
+    {
+        public SerialPort port;
+        byte[] outByteData,inByteData;
+        private bool GotAllWord;
+        StringBuilder stringBuilder;
+        CancellationTokenSource cancelTokenSource;
+            
+        public Client()
+        {
+            cancelTokenSource = new CancellationTokenSource();
+            stringBuilder = new StringBuilder();
+            Task checkAllReceivedTask = new Task(CheckString, cancelTokenSource.Token);
+            checkAllReceivedTask.Start();
+            SetUpConnection();
+        }
+
+        public void SendMessage(string outData)
+        {
+            outByteData=System.Text.Encoding.UTF8.GetBytes(outData);
+            port.Write(outByteData,0,outData.Length);
+        }
+        
+        void SetUpConnection()
+        {
+            port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+            port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+            port.NewLine = "\n";
+            port.Open();
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            lock (stringBuilder)
+            {
+                SerialPort tempPort = (SerialPort)sender;
+                inByteData = new byte[tempPort.BytesToRead];
+                tempPort.Read(inByteData, 0, inByteData.Length);
+                string inData = Encoding.UTF8.GetString(inByteData);
+                stringBuilder.Append(inData.Split("\0").First());
+            }
+        }
+
+        private void CheckString()
+        {
+            while (true)
+            {
+                string data = stringBuilder.ToString();
+                if (data!="" && (data.First() > 48 || data.First() < 58))
+                {
+                    data = data.Remove(0, 1);
+                    if ((data.Contains("Cellcom") || "DONE" == data || "BYE" == data))
+                    {
+                        Console.WriteLine($"Server: {stringBuilder.ToString()}");
+                        stringBuilder.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CellcomClient;
+
+string inputString;
+Client port = new Client();
+
+Console.WriteLine("You need to enter on  format <id>-<action>\t *To quit use id: 999");
+Console.WriteLine("Available actions:\n1. NEW\n2. JOIN\n3. STOP\n4. QUIT");
+Console.WriteLine("Enter to start conversation");
+Console.ReadKey();
+Console.WriteLine("Conversation started:");
+
+while (true)
+{
+    inputString = Console.ReadLine();
+    inputString = inputString.ToUpper();
+    if (inputString.Split("-").Last() == "QUIT")
+    {
+        port.SendMessage(inputString);
+        break;
+    }
+    if(CheckCorrectInput(inputString))
+        port.SendMessage(inputString);
+    else
+        Console.WriteLine("Enter again your request by the correct format");   
+}
+
+bool CheckCorrectInput(string input)
+{
+    if (!input.Contains("-"))
+        return false;
+    if (!input.Contains("NEW") && !input.Contains("JOIN") && !input.Contains("STOP"))
+        return false;
+    return true;
+}
